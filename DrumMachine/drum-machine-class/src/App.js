2839ffbd24e9fc01da79f8d321b1d69a126923b9
kalevnakah <calebschmidt@gmail.com>
2022-09-21 17:40:40 -0500

    Drum Machine: Fixed Install

--- DrumMachine/drum-machine-class/src/App.js
+++ DrumMachine/drum-machine-class/src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import DrumPad from './components/DrumPad';
+import
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Drum Machine
+      <DrumPad />
+    </div>
+  );
 }
 
 export default App;
